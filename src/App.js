d8433ded06f354079f30dddeb0ca9540d0829815
kadir-zeren <k4dzeren@gmail.com>
2023-12-10 17:34:13 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
+import ContainerCard from "./components/ContainerCard";
 
 function App() {
   return (
-    <Container>
+    <Container className="text-center mt-5">
       <Header />
+      <ContainerCard />
     </Container>
   );
 }
