ef2d64e0a7d10d9d2509356825ade3a7860f0721
kadir-zeren <k4dzeren@gmail.com>
2023-12-10 19:16:37 +0300

    v5

--- src/components/ContainerCard.jsx
+++ src/components/ContainerCard.jsx
@@ -27,7 +27,7 @@ const ContainerCard = () => {
         onChange={handleChange}
       />
       <Container className="p-3 rounded-4 card-container my-3">
-        <Row xs={2} md={4} lg={6} className="justify-content-center g-3">
+        <Row xs={1} sm={2} md={3} lg={5} className="justify-content-center g-3">
           {filteredData.map((player, i) => (
             <PlayerCard key={i} {...player} />
           ))}
--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,20 +1,29 @@
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import { ListGroup } from "react-bootstrap";
+import { useState } from "react";
 
 const PlayerCard = ({ name, img, statistics }) => {
+  const [showImage, setShowImage] = useState(true);
   return (
     <Col>
-      <Card>
-        <Card.Img variant="top" src={img} height="350px" />
-        <ul>
-          {statistics.map((item, i) => (
-            <li className="list-unstyled h5 text-start" key={i}>
-              🏀
-              {item}
-            </li>
-          ))}
-        </ul>
+      <Card
+        onClick={() => setShowImage(!showImage)}
+        className="player-card"
+        role="button"
+      >
+        {showImage ? (
+          <Card.Img variant="top" src={img} height="350px" />
+        ) : (
+          <ul className="m-auto">
+            {statistics.map((item, i) => (
+              <li className="list-unstyled h5 text-start" key={i}>
+                🏀
+                {item}
+              </li>
+            ))}
+          </ul>
+        )}
         <Card.Footer>
           <Card.Title>{name}</Card.Title>
         </Card.Footer>
