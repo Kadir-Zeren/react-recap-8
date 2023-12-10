9c194b096887bbb7381511fc4df27ab2f54c2fc6
kadir-zeren <k4dzeren@gmail.com>
2023-12-10 17:28:34 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
+
+export default App;
